d6928471230e0a877aef6f736e2278b65258b365
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2025-08-20 18:39:18 +0200

    Upgrade

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='sphinx-bioschemas',
-    version='0.1',
-    packages=['sphinx_bioschemas'],
-    package_dir={'': 'src'},
+    name="sphinx-bioschemas",
+    version="0.1.0",
+    packages=["sphinx-bioschemas"],
     install_requires=[
-        'sphinx',
+        "sphinx",
     ],
 )
