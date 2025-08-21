4fa3aa443d67ab226eaada36ee32d927d20d7be0
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2025-08-21 12:48:04 +0200

    improving doc and more

--- sphinx-bioschemas/__init__.py
+++ sphinx-bioschemas/__init__.py
@@ -102,9 +102,7 @@ def setup(app: Sphinx):
     """
     Setup function for the sphinx-bioschemas extension.
     """
-    # app.add_config_value("bioschemas_context", "http://schema.org", "html")
     app.add_directive("bioschemas", BioschemasDirective)
-    # app.connect("html-page-context", add_bioschemas_markup)
     return {
         "version": __version__,
         "parallel_read_safe": True,
